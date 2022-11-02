b385332c1d55467f301edf28d739ea14f4908c9b
fortunedavis <fortune.kponou@gmail.com>
2022-11-02 15:00:54 +0100

    good

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  root "home#index"
+
+  get 'omniauth_test', to: 'home#display_omniauth'
+
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
+  devise_scope :user do
+  get 'sign_out' => 'devise/sessions#destroy'
+  end
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+end
