a7bd5f762c48e9c3c29190f44272fd6b505c4099
fortunedavis <fortune.kponou@gmail.com>
2022-11-03 12:58:37 +0100

    ok

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,9 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.17.1"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
-
+set :application, "fongbevi"
+set :repo_url, "git@github.com:fortunedavis/fongbevi.git"
+set :branch, "main"
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
