5ec9f16c19d556186334c8ada5f79b1b57cdfb8f
fortunedavis <fortune.kponou@gmail.com>
2023-04-04 11:35:28 +0100

    cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.17.1"
 
-set :application, "fongbevi"
+set :application, "myapp"
 set :repo_url, "git@github.com:fortunedavis/fongbevi.git"
 set :branch, "main"
 # Default branch is :master
